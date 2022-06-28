74d635f08f26f9585af304f10f7d337c0256a4a6
plalgiush <ilyasushko@yandex.ru>
2022-06-28 21:22:49 +0300

    version pre tasks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-const App = (props) => {
-  const { notes } = props
+import Note from "./components/Note";
 
+const App = ({ notes }) => {
   return (
       <div>
-        <h1>Notes</h1>
-        <ul>
-          <li>{notes[0].content}</li>
-          <li>{notes[1].content}</li>
-          <li>{notes[2].content}</li>
-        </ul>
+         <h1>Notes</h1>
+         <ul>
+             {notes.map(note =>
+                 <Note key={note.id} note={note} />
+             )}
+         </ul>
       </div>
   )
 }
