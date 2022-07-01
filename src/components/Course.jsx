f044a7fdca8750b80070df38aea56a1ff1ca97a6
plalgiush <ilyasushko@yandex.ru>
2022-07-01 14:46:15 +0300

    added correct operability

---
+++ src/components/Course.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Name from "./Name";
+import Total from "./Total";
+
+const Course = ({ courses }) => {
+    return (
+        <div>
+            <h1>Web development curriculum</h1>
+            {courses.map(course =>
+                <div>
+                    <h2>{course.name}</h2>
+                    <div>
+                        <Name course={course} />
+                    </div>
+                    <p>
+                        <Total parts={course.parts}/>
+                    </p>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Course;
