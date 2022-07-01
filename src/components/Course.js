ad08a9f196afe2a53674c078012cd2850d001bc9
plalgiush <ilyasushko@yandex.ru>
2022-07-01 14:18:15 +0300

    final version the task of 2.4 - 2.5

--- src/components/Course.js
+++ src/components/Course.js
@@ -2,17 +2,19 @@ import React from 'react';
 import Name from "./Name";
 import Total from "./Total";
 
-const Course = ({ course }) => {
+const Course = ({ courses }) => {
     return (
-        <div>
-             <h1>{course.name}</h1>
-             <div>
-                 <Name course={course} />
-             </div>
-             <p>
-                 <Total parts={course.parts}/>
-             </p>
-        </div>
+        courses.map(course =>
+            <div>
+                <h1>{course.name}</h1>
+                <div>
+                    <Name course={course} />
+                </div>
+                <p>
+                    <Total parts={course.parts}/>
+                </p>
+            </div>
+        )
     );
 };
 
