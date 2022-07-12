bcdbb15e5f9a3195cb6db50ef7403ca61175be1c
plalgiush <ilyasushko@yandex.ru>
2022-07-12 11:53:08 +0300

    final version the task of 2.7

--- src/App.js
+++ src/App.js
@@ -1,16 +1,52 @@
+import { useState } from 'react'
 import Note from "./components/Note";
 
-const App = ({ notes }) => {
-  return (
-      <div>
-         <h1>Notes</h1>
-         <ul>
-             {notes.map(note =>
-                 <Note key={note.id} note={note} />
-             )}
-         </ul>
-      </div>
-  )
+const App = () => {
+    const [numbers, setNumbers] = useState([])
+    const [newName, setNewName] = useState('')
+
+    const handleNameChange = (event) => {
+        setNewName(event.target.value)
+    }
+
+    const addName = (event) => {
+        event.preventDefault()
+        const numberObject = {
+            content: newName,
+            id: numbers.length + 1,
+        }
+
+        const readyName = numbers.find(number => newName === number.content);
+
+        if (readyName !== undefined) {
+            alert(`${newName} is already added to phonebook`)
+        } else {
+            setNumbers(numbers.concat(numberObject))
+            setNewName('')
+        }
+    }
+
+
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form onSubmit={addName}>
+                <div>
+                    name: <input value={newName} onChange={handleNameChange} />
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            <div>
+                {numbers.map(note =>
+                    <Note key={note.id} note={note} />
+                )}
+            </div>
+        </div>
+    )
 }
 
 export default App
--- src/components/Note.js
+++ src/components/Note.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Note = ({ note }) => {
     return (
-        <li>{note.content}</li>
+        <div>{note.content}</div>
     );
 };
 
